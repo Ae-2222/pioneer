c990aa8ce048d80f96302e2811f4732a39a22e3a
Robert Norris <rob@eatenbyagrue.org>
2012-10-12 10:04:54 +1100

    naive port of ShipSpinnerWidget to new-ui, untested

--- src/gameui/ShipSpinner.cpp
+++ src/gameui/ShipSpinner.cpp
@@ -1,1 +1,55 @@
 #include "ShipSpinner.h"
+#include "Ship.h"
+#include "Pi.h"
+
+using namespace UI;
+
+namespace GameUI {
+
+void ShipSpinner::Layout()
+{
+	m_model = LmrLookupModelByName(ShipType::types[m_flavour.type].lmrModelName.c_str());
+
+	memset(&m_params, 0, sizeof(LmrObjParams));
+	m_params.animationNamespace = "ShipAnimation";
+	m_params.equipment = &m_equipment;
+	m_flavour.ApplyTo(&m_params);
+	m_params.animValues[Ship::ANIM_WHEEL_STATE] = 1.0;
+	m_params.flightState = Ship::FLYING;
+
+	Color lc(0.5f, 0.5f, 0.5f, 0.f);
+	m_light.SetDiffuse(lc);
+	m_light.SetAmbient(lc);
+	m_light.SetSpecular(lc);
+	m_light.SetPosition(vector3f(1.f, 1.f, 0.f));
+	m_light.SetType(Graphics::Light::LIGHT_DIRECTIONAL);
+}
+
+void ShipSpinner::Draw()
+{
+	float rot1 = 0.0f, rot2 = 0.0f;
+
+	Point pos(GetAbsolutePosition());
+	Point size(GetSize());
+
+	Graphics::Renderer *r = GetContext()->GetRenderer();
+
+	Graphics::Renderer::StateTicket ticket(r);
+
+	r->SetPerspectiveProjection(45.f, 1.f, 1.f, 10000.f);
+	r->SetTransform(matrix4x4f::Identity());
+
+	r->SetDepthTest(true);
+	r->ClearDepthBuffer();
+
+	r->SetLights(1, &m_light);
+	r->SetViewport(pos.x, GetContext()->GetSize().y - pos.y, size.x, size.y);
+
+	matrix4x4f rot = matrix4x4f::RotateXMatrix(rot1);
+	rot.RotateY(rot2);
+	rot[14] = -1.5f * m_model->GetDrawClipRadius();
+
+	m_model->Render(rot, &m_params);
+}
+
+}
--- src/gameui/ShipSpinner.h
+++ src/gameui/ShipSpinner.h
@@ -1,4 +1,33 @@
 #ifndef _GAMEUI_SHIPSPINNER_H
 #define _GAMEUI_SHIPSPINNER_H
 
+#include "ui/Context.h"
+#include "ShipFlavour.h"
+#include "graphics/Light.h"
+#include "LmrModel.h"
+#include "EquipSet.h"
+
+namespace GameUI {
+
+class ShipSpinner : public UI::Widget {
+public:
+	ShipSpinner(UI::Context *context, const ShipFlavour &flavour) : UI::Widget(context), m_flavour(flavour) {}
+
+	virtual void Layout();
+	virtual void Draw();
+
+private:
+	ShipFlavour m_flavour;
+
+	LmrModel *m_model;
+	LmrObjParams m_params;
+	// XXX m_equipment is currently not hooked up to anything,
+	// it's just used to pass equipment parameters to the displayed model
+	EquipSet m_equipment;
+	//illumination in ship view
+	Graphics::Light m_light;
+};
+
+}
+
 #endif
