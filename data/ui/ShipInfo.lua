b870163cd216945c2f99694d7f36d9f18c9fbabd
Robert Norris <rob@eatenbyagrue.org>
2012-10-15 08:40:14 +1100

    preparing data tables

--- data/ui/ShipInfo.lua
+++ data/ui/ShipInfo.lua
@@ -7,7 +7,23 @@ ui.templates.ShipInfo = function (args)
 	return ui:Background():SetInnerWidget(ui:Margin(30):SetInnerWidget(
 		ui:Grid(2,1)
 			:SetColumn(0, {
-				ui:VBox(10):PackEnd({
+				ui:VBox(20):PackEnd({
+					ui:Label("Ship information"):SetFontSize("LARGE"),
+					ui:Grid(2,1)
+						:SetColumn(0, {
+							ui:VBox():PackEnd({
+								ui:Label("Hyperdrive:"):SetFontSize("SMALL"),
+								ui:Label("Hyperspace range:"):SetFontSize("SMALL"),
+								ui:Margin(10),
+								ui:Label("Capacity:"):SetFontSize("SMALL"),
+								ui:Label("Free:"):SetFontSize("SMALL"),
+								ui:Label("Used:"):SetFontSize("SMALL"),
+								ui:Label("All-up weight:"):SetFontSize("SMALL"),
+								ui:Margin(10),
+								ui:Label("Front weapon:"):SetFontSize("SMALL"),
+								ui:Label("Rear weapon:"):SetFontSize("SMALL")
+							})
+						})
 				})
 			})
 			:SetColumn(1, {
