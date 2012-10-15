c9cce7d5fc4b83fe3ea36ae0d24d87d5474ff8a6
Robert Norris <rob@eatenbyagrue.org>
2012-10-15 15:49:46 +1100

    equipment

--- data/ui/ShipInfo.lua
+++ data/ui/ShipInfo.lua
@@ -13,14 +13,44 @@ ui.templates.ShipInfo = function (args)
 
 	local stats = Game.player:GetStats()
 
+	local equipColumn = { {}, {} }
+	local t = 1
+	for i = 1,#Constants.EquipType do
+		local type = Constants.EquipType[i]
+		local et = EquipType.GetEquipType(type)
+		local slot = et.slot
+		if (slot ~= "CARGO" and slot ~= "MISSILE" and slot ~= "ENGINE" and slot ~= "LASER") then
+			local count = Game.player:GetEquipCount(slot, type)
+			if count > 0 then
+				if count > 1 then
+					if type == "SHIELD_GENERATOR" then
+						table.insert(equipColumn[t],
+							ui:Label(string.interp("{quantity} Shield Generators", { quantity = string.format("%d", count) })))
+					elseif type == "PASSENGER_CABIN" then
+						table.insert(equipColumn[t],
+							ui:Label(string.interp("{quantity} Occupied Passenger Cabins", { quantity = string.format("%d", count) })))
+					elseif type == "UNOCCUPIED_CABIN" then
+						table.insert(equipColumn[t],
+							ui:Label(string.interp("{quantity} Unoccupied Passenger Cabins", { quantity = string.format("%d", count) })))
+					else
+						table.insert(equipColumn[t], ui:Label(et.name))
+					end
+				else
+					table.insert(equipColumn[t], ui:Label(et.name))
+				end
+				t = t == 1 and 2 or 1
+			end
+		end
+	end
+
 	return ui:Background():SetInnerWidget(ui:Margin(30):SetInnerWidget(
 		ui:Grid(2,1)
 			:SetColumn(0, {
 				ui:VBox(20):PackEnd({
 					ui:Label("Ship information"):SetFontSize("LARGE"),
-					ui:Grid(2,1)
+					ui:Grid(2,1):SetFontSize("SMALL")
 						:SetColumn(0, {
-							ui:VBox():SetFontSize("SMALL"):PackEnd({
+							ui:VBox():PackEnd({
 								ui:Label("Hyperdrive:"),
 								ui:Label("Hyperspace range:"),
 								ui:Margin(10),
@@ -34,7 +64,7 @@ ui.templates.ShipInfo = function (args)
 							})
 						})
 						:SetColumn(1, {
-							ui:VBox():SetFontSize("SMALL"):PackEnd({
+							ui:VBox():PackEnd({
 								ui:Label(EquipType.GetEquipType(hyperdrive).name),
 								ui:Label(string.interp(
 									"{range} light years ({maxRange} max)", {
@@ -51,7 +81,11 @@ ui.templates.ShipInfo = function (args)
 								ui:Label(EquipType.GetEquipType(frontWeapon).name),
 								ui:Label(EquipType.GetEquipType(rearWeapon).name),
 							})
-						})
+						}),
+					ui:Label("Equipment"),
+					ui:Grid(2,1):SetFontSize("SMALL")
+						:SetColumn(0, { ui:VBox():PackEnd(equipColumn[1]) })
+						:SetColumn(1, { ui:VBox():PackEnd(equipColumn[2]) })
 				})
 			})
 			:SetColumn(1, {
